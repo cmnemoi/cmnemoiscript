db08c979d38bebe75cd9800b0eb22dd55d123d11
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2024-06-28 01:46:33 +0200

    feat(lexer): Tokenize integer affectations (`foo = 42`) (#4)
    * Add first WIP lexer, which can tokenize `x = 42`
    
    * feat: Generalize lexer to all affectations
    
    * refactor: use a match statement instead of if/else (Rust magic!)
    
    * chore: release 0.1.0
    
    Release-As: 0.1.0

---
+++ src/lexer.rs
@@ -0,0 +1,80 @@
+fn tokenize(source_code: &str) -> Vec<Token> {
+    source_code
+        .split(' ')
+        .map(get_token_from)
+        .collect::<Vec<Token>>()
+}
+
+fn get_token_from(character: &str) -> Token {
+    match character {
+        _ if character.parse::<f64>().is_ok() => Token {kind: TokenType::Number, value: character.to_string()},
+        "=" => Token {kind: TokenType::Equals, value: character.to_string()},
+        _ if character.is_ascii() => Token {kind: TokenType::Variable, value: character.to_string()},
+        _ => panic!("Syntax error: invalid token {}", character),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenType {
+    Variable,
+    Equals,
+    Number,
+}
+
+#[derive(Debug, PartialEq)]
+struct Token {
+    kind: TokenType,
+    value: String,
+}
+#[cfg(test)]
+mod tests {
+    use crate::lexer::{tokenize, Token, TokenType};
+
+    #[test]
+    fn should_tokenize_x_equals_42() {
+        let expected_tokens = vec![
+            Token {kind: TokenType::Variable, value: "x".to_string()},
+            Token {kind: TokenType::Equals, value: "=".to_string()},
+            Token {kind: TokenType::Number, value: "42".to_string()},
+        ];
+        let actual_tokens = tokenize("x = 42");
+
+        assert_eq!(actual_tokens, expected_tokens)
+    }
+
+    #[test]
+    fn should_tokenize_x_equals_43() {
+        let expected_tokens = vec![
+            Token {kind: TokenType::Variable, value: "x".to_string()},
+            Token {kind: TokenType::Equals, value: "=".to_string()},
+            Token {kind: TokenType::Number, value: "43".to_string()},
+        ];
+        let actual_tokens = tokenize("x = 43");
+
+        assert_eq!(actual_tokens, expected_tokens)
+    }
+
+    #[test]
+    fn should_tokenize_y_equals_42() {
+        let expected_tokens = vec![
+            Token {kind: TokenType::Variable, value: "y".to_string()},
+            Token {kind: TokenType::Equals, value: "=".to_string()},
+            Token {kind: TokenType::Number, value: "42".to_string()},
+        ];
+        let actual_tokens = tokenize("y = 42");
+
+        assert_eq!(actual_tokens, expected_tokens)
+    }
+
+    #[test]
+    fn should_tokenize_xy_equals_42() {
+        let expected_tokens = vec![
+            Token {kind: TokenType::Variable, value: "xy".to_string()},
+            Token {kind: TokenType::Equals, value: "=".to_string()},
+            Token {kind: TokenType::Number, value: "42".to_string()},
+        ];
+        let actual_tokens = tokenize("xy = 42");
+
+        assert_eq!(actual_tokens, expected_tokens)
+    }
+}
